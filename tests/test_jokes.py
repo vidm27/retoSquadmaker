d138b971086a374339e9d3677ae7589fc493d640
vidm27 <davidmoran464@gmail.com>
2022-07-29 11:15:04 -0500

    test to select joke and no support joke

---
+++ tests/test_jokes.py
@@ -0,0 +1,19 @@
+import pytest
+from fastapi import HTTPException
+from fastapi.testclient import TestClient
+
+from api_jokes import api_jokes, select_joke
+from main import app
+
+client = TestClient(app)
+
+def test_has_not_select_joke():
+  with pytest.raises(KeyError):
+    select_joke('Joke')
+    
+def test_not_suppoted_joke():
+  response = client.get('/jokes/Data')
+  detail_error = {"detail":"This type of joke is not supported"}
+  assert response.status_code == 400
+  assert response.json() == detail_error
+    
